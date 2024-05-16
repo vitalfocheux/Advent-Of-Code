4901b5bab0009e5a8c942445e298cb4d42238bc0
vitalfocheux <vitalfocheux@gmail.com>
2024-05-16 15:53:50 +0200

    Day 01 to 14

---
+++ dist/assets/y2019-BcWnOXlK.js
@@ -0,0 +1,1 @@
+import{B as r}from"./ButtonDay-Cq7KHkSN.js";import{N as a}from"./Navbar-DzpSwXSp.js";import{o,c as s,b as e,F as n,u}from"./index-DqfKCZJy.js";const c={setup(){return{router:u()}}},i=Object.assign(c,{__name:"y2019",setup(t){return(_,m)=>(o(),s(n,null,[e(a),e(r,{year:"2019"})],64))}});export{i as default};

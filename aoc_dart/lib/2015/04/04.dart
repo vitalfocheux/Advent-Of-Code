adf8603e1202eb1ae98b82a63336bd1f29a0b23d
vitalfocheux <vital.focheux@edu.univ-fcomte.fr>
2024-10-13 15:35:15 +0200

    Day 04

---
+++ aoc_dart/lib/2015/04/04.dart
@@ -0,0 +1,21 @@
+import '../../packages/models.dart';
+import 'dart:convert';
+import 'package:crypto/crypto.dart';
+
+void main() async {
+  AOCD aocd = await AOCD.create(2015, 4);
+  print(part(aocd.asStr, '00000'));
+  print(part(aocd.asStr, '000000'));
+}
+
+int part(String parser, String lowest){
+  int result = 0;
+  while(true){
+    String to_hash = parser + result.toString();
+    if(md5.convert(utf8.encode(to_hash)).toString().startsWith(lowest)){
+      break;
+    }
+    result++;
+  }
+  return result;
+}

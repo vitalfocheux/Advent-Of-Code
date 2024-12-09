a10a1ec438c2df262cdd166ea06a0c57fc99c3e1
vitalfocheux <vital.focheux@edu.univ-fcomte.fr>
2024-12-09 17:45:10 +0100

    part2

--- aoc_dart/lib/2024/09/09.dart
+++ aoc_dart/lib/2024/09/09.dart
@@ -3,7 +3,7 @@ import 'package:aoc_dart/packages/extension/string_extension.dart';
 
 void main() async {
   AOCD aocd = await AOCD.create(2024, 9);
-  // print(part1(aocd.asStr));
+  print(part1(aocd.asStr));
   print(part2(aocd.asStr));
 }
 
@@ -56,96 +56,70 @@ void space(List<String> disk_map, List<String> disk, int index_map, int ID, bool
 part2(String input){
   num res = 0;
   List<String> disk_map = input.split('');
-  List<String> disk = [];
-  Map<int, int> length_blocks = {};
-  Map<int, (int, int)> length_free_space = {};
-  space_2(disk_map, disk, 0, 0, false, length_blocks, length_free_space);
-  // print(disk);
-  move_2(disk, length_blocks, length_free_space);
-  for(int i = 0; i < disk.length; i++){
-    if(disk[i] == '.'){
+  List<int?> disk = [];
+  space_2(disk_map, disk);
+  for(int i = disk.length-1; i > 0; i--){
+    if(disk[i] == null){
       continue;
     }
-    res += i * disk[i].toInt();
+    int curr = disk[i]!;
+    int curr_size = 1;
+    for(int j = i-1; j >= 0; j--){
+      if(disk[j] != curr){
+        break;
+      }
+      curr_size++;
+    }
+    while(true){
+      int first_free_index = disk.indexWhere((elt) => elt == null);
+      if(first_free_index > i || first_free_index == -1){
+        i -= curr_size-1;
+        break;
+      }
+      int empty_space_size = disk.sublist(first_free_index, disk.indexWhere((elt) => elt != null, first_free_index)).length;
+      if(empty_space_size < curr_size){
+        for(int j = 0; j < empty_space_size; j++){
+          disk[first_free_index+j] = -1;
+        }
+        continue;
+      }
+      for(int j = 0; j < curr_size; j++){
+        disk[i-j] = null;
+        disk[first_free_index+j] = curr;
+      }
+      i -= curr_size-1;
+      break;
+    }
+    for(int j = 0; j < disk.length; j++){
+      if(disk[j] == -1){
+        disk[j] = null;
+      }
+    }
+  }
+  for(int i = 0; i < disk.length; i++){
+    if(disk[i] == null){
+      continue;
+    }
+    res += i * disk[i]!;
   }
   return res;
 }
 
-void space_2(List<String> disk_map, List<String> disk, int index_map, int ID, bool is_free_space, Map<int, int> length_blocks, Map<int, (int, int)> length_free_space){
-  for(int i = 0; i < disk_map.length; i++){
-    int index = disk_map[i].toInt();
-    if(is_free_space){
-      int disk_length = disk.length;
-      for(int j = 0; j < index; j++){
-        disk.add('.');
+void space_2(List<String> disk_map, List<int?> disk){
+  bool freeSpace = false;
+  int id = 0;
+  for (final char in disk_map) {
+    int value = int.parse(char);
+    for (int i = 0; i < value; i++) {
+      if (freeSpace) {
+        disk.add(null);
+      } else {
+        disk.add(id);
       }
-      length_free_space[length_free_space.length] = (disk_length, index);
-    }else{
-      for(int j = 0; j < index; j++){
-        disk.add(ID.toString());
-      }
-      length_blocks[ID] = index;
-      ID++;
     }
-    is_free_space = !is_free_space;
+    if (freeSpace) {
+      id++;
+    }
+    freeSpace = !freeSpace;
   }
 }
-
-void move_2(List<String> disk, Map<int, int> length_blocks, Map<int, (int, int)> length_free_space){
-  Set<int> visited = {};
-  for(int i = disk.length-1; i > 0; i--){
-    print("$i");
-    print(length_free_space);
-    print(length_blocks);
-    print(disk);
-    if(disk[i] == '.'){
-      continue;
-    }
-    int ID = disk[i].toInt();
-    if(visited.contains(ID)){
-      continue;
-    }
-    print("ID $ID");
-    int length_ID = length_blocks[ID]!;
-    int length_dot = -1;
-    int index_dot = -1;
-    int free_space = -1;
-    for(int j = 0; j < length_free_space.length; ++j){
-      if(length_free_space[j] == null){
-        // print("null $j");
-        continue;
-      }
-      if(length_free_space[j]!.$2 >= length_ID){
-        print(j);
-        length_dot = length_free_space[j]!.$2;
-        index_dot = length_free_space[j]!.$1;
-        free_space = j;
-        break;
-      }
-    }
-    if(length_dot == -1){
-      i -= length_ID;
-      continue;
-    }
-    for(int j = 0; j <length_ID; j++){
-      disk[index_dot+j] = ID.toString();
-      disk[i] = '.';
-      i--;
-    }
-    i++;
-    visited.add(ID);
-    if(disk[index_dot+length_ID] == '.'){
-      int new_length_dot = length_dot-length_ID;
-      print("new_length_dot $length_dot - $length_ID = $new_length_dot");
-      if(new_length_dot > 0){
-        length_free_space.update(free_space, (value) => (index_dot+length_ID, new_length_dot));
-      }else{
-        length_free_space.remove(free_space);
-      }
-    }else{
-      length_free_space.remove(free_space);
-    }
-  }
-}
-
-//6396171990508 is too high
